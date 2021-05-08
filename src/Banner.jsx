69e76f97f657e22ea9de591cff4b84346ebc6c2e
zerozoo-front <zerozoo385@gmail.com>
2021-05-09 00:18:55 +0900

    fix media query

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -2,21 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 const BannerStyle = styled.div`
-  width: auto;
   background-image: url('https://a0.muscache.com/im/pictures/b65bef33-07be-4c55-b613-bb990193e8f6.jpg?im_w=1440');
   background-size: cover;
   background-repeat: no-repeat;
   resize: both;
   background-position-y: 90%;
   & .script {
-    width: 90%;
     white-space: pre-wrap;
     padding-top: 45%;
     padding-bottom: 40%;
     padding-left: 5%;
-  }
-  & .script h1 {
-    min-width: 7rem;
   }
   & .bannerBtn {
     background: black;
@@ -29,30 +24,128 @@ const BannerStyle = styled.div`
     border-radius: 0.6rem;
     cursor: pointer;
   }
-  @media screen and (min-width: 949px) {
+  @media screen and (min-width: 1440px) {
+    height: 34.375rem;
     & .script {
-      width: 25%;
+      width: 20.8475rem;
+      white-space: pre-wrap;
+      padding-top: 10%;
+      padding-bottom: 40%;
+      padding-left: 15%;
+      h1 {
+        font-size: 3.75rem;
+        width: 13.9375rem;
+      }
+      .secondScript {
+        display: flex;
+        flex-direction: column;
+        width: 23rem;
+        font-size: 1.25rem;
+      }
+    }
+    & .bannerBtn {
+      background: black;
+      width: 9.438rem;
+      color: white;
+      text-align: center;
+      height: 2.2rem;
+      line-height: 2rem;
+      font-size: 1rem;
+      border-radius: 0.6rem;
+      cursor: pointer;
+      margin-top: 0.9rem;
+    }
+  }
+  @media screen and (max-width: 1439px) {
+    height: 34.375rem;
+    & .script {
+      width: 20.8475rem;
+      white-space: pre-wrap;
+      padding-top: 10%;
+      padding-bottom: 40%;
+      padding-left: 15%;
+      h1 {
+        font-size: 3.75rem;
+        width: 13.9375rem;
+      }
+      .secondScript {
+        display: flex;
+        width: 23rem;
+        font-size: 1.25rem;
+      }
+    }
+    & .bannerBtn {
+      background: black;
+      width: 9.438rem;
+      color: white;
+      text-align: center;
+      height: 2.2rem;
+      line-height: 2rem;
+      font-size: 1rem;
+      border-radius: 0.6rem;
+      cursor: pointer;
+      margin-top: 0.9rem;
+    }
+  }
+  @media screen and (max-width: 949px) {
+    height: auto;
+    & .script {
+      width: 20.8475rem;
       white-space: pre-wrap;
       padding-top: 30%;
       padding-bottom: 40%;
       padding-left: 15%;
-      font-size: 2rem;
-    }
-    & .script h1 {
-      min-width: 7rem;
+      h1 {
+        font-size: 2.8125rem;
+        width: 10.8475rem;
+      }
+      .secondScript {
+        display: flex;
+      }
     }
     & .bannerBtn {
       background: black;
-      width: 8.438rem;
+      width: 9.438rem;
       color: white;
       text-align: center;
-      height: 2rem;
+      height: 2.2rem;
       line-height: 2rem;
-      font-size: 0.8rem;
+      font-size: 1rem;
+      border-radius: 0.6rem;
+      cursor: pointer;
+      margin-top: 0.9rem;
+    }
+  }
+  @media screen and (max-width: 744px) {
+    height: auto;
+    & .script {
+      width: 17.5rem;
+      height: 12.375rem;
+      white-space: pre-wrap;
+      padding-top: 30%;
+      padding-bottom: 40%;
+      padding-left: 15%;
+      h1 {
+        font-size: 2.8125rem;
+        width: 10rem;
+      }
+      .secondScript {
+        display: flex;
+        flex-direction: column;
+      }
+    }
+    & .bannerBtn {
+      background: black;
+      width: 9.438rem;
+      color: white;
+      text-align: center;
+      height: 2.2rem;
+      line-height: 2rem;
+      font-size: 1rem;
       border-radius: 0.6rem;
       cursor: pointer;
     }
-  } ;
+  }
 `;
 
 const Banner = () => (
@@ -60,7 +153,10 @@ const Banner = () => (
     <BannerStyle>
       <div className='script'>
         <h1>슬기로운 자연생활</h1>
-        <p>에어비앤비가 엄선한 위시리스트를 만나보세요.</p>
+        <div className='secondScript'>
+          <span>에어비앤비가 엄선한 </span>
+          <span>위시리스트를 만나보세요.</span>
+        </div>
         <div className='bannerBtn'>여행 아이디어 얻기</div>
       </div>
     </BannerStyle>
